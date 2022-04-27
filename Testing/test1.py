eb527db6a9b64b7137cb2de69fc93aa0933809e8
mimo <qa.softwareteam@gmail.com>
2022-04-26 22:00:01 -0300

    Hello World with chrome driver

--- Testing/test1.py
+++ Testing/test1.py
@@ -2,12 +2,15 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
+#Ruta del driver(.exe)
 chrome_driver_path = 'C:\\Users\\tntau\\Python-Automation\\Drivers\\chromedriver.exe'
 gecko_driver_path = './Drivers/geckodriver'
 
+#Direccion de la pagina
 url = 'https://www.youtube.com/'
 service = Service(chrome_driver_path)
 
+#mediante navegador ...
 driver = webdriver.Chrome(service=service)
 driver.get(url)
 time.sleep(3)
